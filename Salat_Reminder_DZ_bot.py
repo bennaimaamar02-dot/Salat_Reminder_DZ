7c6946a5939588ae3b3fa69435ffa7700499f0e0
bennaimaamar02-dot <bennaimaamar02@gmail.com>
2026-09-20 12:56:11 +0100

    Update Salat_Reminder_DZ_bot.py

--- Salat_Reminder_DZ_bot.py
+++ Salat_Reminder_DZ_bot.py
@@ -1,4 +1,27 @@
 import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return 'Bot is alive!'
+
+
+def run():
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
+
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
+
+
+# استدعاء الدالة قبل تشغيل البوت
+keep_alive()
 import datetime
 import logging
 import requests
